f278b01d041a2f62de1756447ac3803234fddca5
Henrik Hoff <henrikln@gmail.com>
2013-08-22 16:50:13 +0200

    Added setting to save what pages are to be used to display the plugin content. This is needed to know what page to link to when linking between lecturers, courses and the schedule.

--- wpcm_plugin_options.php
+++ wpcm_plugin_options.php
@@ -19,13 +19,84 @@ function WPCM_plugin_options() {
 	if (!current_user_can('manage_options'))  {
 		wp_die( __('You do not have sufficient permissions to access this page.') );
 	}
-	echo '<div class="wrap">';
+
+	$lecturer_page = $_REQUEST['lecturer_page'];
+	$course_page = $_REQUEST['course_page'];
+	$schedule_page = $_REQUEST['schedule_page'];
+	
+	echo $lecturer_page.$course_page.$schedule_page;
+	
+	if ( $lecturer_page != '' || $course_page != '' || $schedule_page != '')
+	{
+		echo '<p>minst ett värde är inte tomt</p>';
+		update_option('wpcm_lecturer_page',$lecturer_page);
+		update_option('wpcm_course_page',$course_page);
+		update_option('wpcm_schedule_page',$schedule_page);	
+	}
+
+	$lecturer_page = get_option('wpcm_lecturer_page');
+	$course_page = get_option('wpcm_course_page');
+	$schedule_page = get_option('wpcm_schedule_page');
+
+	echo '<div class="WPCM_manager_wrap">';
 	echo '<h2>'.__('WordPress Course Manager').'</h2>';
-	echo '<p>'.__('Here is where any plugin settings should be changed if there where any.').'</p>';
 	echo '<p>'.__('To manage the plugin content, use the links under WP Course Manager in the menu to the left.').'</p>';
+	
+	$all_pages = get_pages();
+	
+	echo '<form id="wpcm_options" method="post" action="admin.php?page='. $_GET['page'].'">';
+	echo '<fieldset>';
+	echo '<legend>'.__("Select the pages below that are used to display the plugin content.").'</legend>';	
+	
+	echo '<p><label>'.__("Page that displays").' <strong>' .__("lecturers").'</strong>: </label>';
+	echo '<select name="lecturer_page">';
+	echo '<option value="">'.__("Select page").'</option>';
+	foreach ($all_pages as $page)
+	{
+		$option = '<option value="' . $page->post_name . '" '. ($page->post_name == $lecturer_page ? 'selected' : '') .'>';
+		$option .= $page->post_title;
+		$option .= '</option>';
+		echo $option;
+	}
+	echo '</select>';
+	echo '<br/>'.__("This page should only contain this shortcode: ") . '[wpcm_lecturers]';
+	echo '</p>';
+	
+	echo '<p><label>'.__("Page that displays").' <strong>' .__("courses").'</strong>: </label>';
+	echo '<select name="course_page">';
+	echo '<option value="">'.__("Select page").'</option>';
+	foreach ($all_pages as $page)
+	{
+		$option = '<option value="' . $page->post_name . '" '. ($page->post_name == $course_page ? 'selected' : '') .'>';
+		$option .= $page->post_title;
+		$option .= '</option>';
+		echo $option;
+	}
+	echo '</select>';
+	echo '<br/>'.__("This page should only contain this shortcode: ") . '[wpcm_courses]';
+	echo '</p>';
+	
+	
+	echo '<p><label>'.__("Page that displays").' <strong>' .__("the schedule").'</strong>: </label>';
+	echo '<select name="schedule_page">';
+	echo '<option value="">'.__("Select page").'</option>';
+	foreach ($all_pages as $page)
+	{
+		$option = '<option value="' . $page->post_name . '" '. ($page->post_name == $schedule_page ? 'selected' : '') .'>';
+		$option .= $page->post_title;
+		$option .= '</option>';
+		echo $option;
+	}
+	echo '</select>';
+	echo '<br/>'.__("This page should only contain this shortcode: ") . '[wpcm_schedule]';
+	echo '</p>';
+	
+	
+	echo '<p><button type="submit">'.__("Save").'</button></p>';	
+	
+	echo '</fieldset></form>';
 	echo '</div>';
 
 }
-
 
 ?>
