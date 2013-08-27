1b290ef66a6cc7b409702003a135bb463ed90205
Henrik Hoff <henrikln@gmail.com>
2013-08-27 15:51:11 +0200

    The plugin now displays info about the courses, as well as the lecturers. Listing all courses in a list and links to specific info about each course, on the page with the courses shortcode. Todo: add links to "more info" url about each scheduled date for a course.

--- wpcm_front-end.php
+++ wpcm_front-end.php
@@ -74,7 +74,7 @@ function WPCM_show_one_lecturer($id) {
 	//$courses_page holds the short-hand url to the page where the shortcode for the courses is placed. 
 	//This is saved with set_option() in the plugin settings, and if that setting is not set by the user, 
 	//this value will be empty. In that case, we will not create any links to the courses.
-	if($courses_page != '')
+	if($courses_page != '')		
 	{
 			foreach ($courses as $course)
 			{
@@ -99,8 +99,96 @@ function WPCM_show_one_lecturer($id) {
 	
 //Triggered by shortcode wpcm_courses (defined in wp-course-manager.php)
 function WPCM_list_courses($conten=null){
-	return "ToDo: List all courses.";	
-	}	
+	$course = $_REQUEST['id'];	
+	
+	if ($course) {
+		$output = WPCM_show_one_course($course);
+	}
+	else {
+		$output = WPCM_list_all_courses();
+	}
+	return $output;	
+	
+}
+
+function WPCM_list_all_courses(){
+	
+	$courses = get_courses();
+	
+	$output = "<div class='WPCM_content WPCM_courses'>";
+	$output .= "<ul>";
+	
+	foreach ($courses as $course)
+	{
+		$output .= "<li><a href='?id=".$course->id."' >".$course->title."</a></li>";	
+	}
+
+	$output .= "</ul></div>";
+
+	return $output;
+
+}	
+
+function WPCM_show_one_course($id) {
+	$course = get_courses($id);
+	$lecturers = get_course_holders($id);
+	$dates = get_schedule($id);
+	
+	$lecturer_page = get_option('wpcm_lecturer_page');	
+	
+	$title = stripslashes($course[0]->title);
+	$short_descr = text2html(stripslashes($course[0]->short_description));
+	$description = text2html(stripslashes($course[0]->description));
+	$target = stripslashes($course[0]->target);
+	
+	$output .= "<div class='WPCM_content WPCM_courses'>";
+	
+	$output .= "<h2>".$course[0]->title."</h2>";
+	$output .= "<div class='WPCM_course_ingress'><p>".$short_descr."</p></div>";
+	
+	$output .= "<h3>".__('Lecturers')."</h3>";
+	
+	foreach ( $lecturers as $lecturer)
+	{
+		$output .= "<div class='WPCM_course_holder'>";
+		$output .= "<a href='".$lecturer_page."?id=".$lecturer->id."'>";
+		$output .= "<img src='".$lecturer->image."' alt='".stripslashes($lecturer->name)."' /><br/>";
+		$output .= stripslashes($lecturer->name)."</a>";
+		$output .= "</div>";
+	}
+	 
+	
+	$output .= "<h3>".__('Description')."</h3>";
+	$output .= "<div class='WPCM_course_description'><p>".$description."</p></div>";
+
+	$output .= "<h3>".__('Target group')."</h3>";	
+	$output .= "<p class='WPCM_course_target'>".$target."</p>";
+	
+	$output .= "<h3>".__('Dates')."</h3>";
+	$output .= "<ul>";
+	
+	if (isset($dates[0]))
+	{
+		foreach ( $dates as $date )
+		{
+			$output .= "<li>".$date->date. ", ".$date->city."</li>";
+		}
+	
+	}
+	else
+	{
+		$output .= "<li>".__('No scheduled dates for this course')."</li>"; 	
+	}
+	
+	
+	
+	$output .= "</ul>";	
+	
+	$output .= "</div>";
+	
+	return $output;	
+	
+}
 
 //Triggered by shortcode wpcm_schedule (defined in wp-course-manager.php)
 function WPCM_list_schedule($content=null){
